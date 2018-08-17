23141c73a3ab536642e767271a5fea800134f7db
Muhamad Visat Sutarno <mvisat@gmail.com>
2018-08-17 19:06:58 +0700

    re-export from index

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import {
+    ErrNotLocked,
+    ErrTimeout,
+    RWLock,
+    State,
+} from './rwlock';
+
+export {
+    ErrNotLocked,
+    ErrTimeout,
+    RWLock,
+    State,
+};
+
+export default RWLock;
